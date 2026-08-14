d678f979acf1ef1183a43560a3c67e56b1699a22
wael3adel-lang <wael3adel@gmail.com>
2026-08-14 20:42:08 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='halab-fuel-v3';
+const CACHE='halab-fuel-v4';
 const ASSETS=['./','./index.html','./admin.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
