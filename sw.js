ce9e63754e5c65fd5e5f6511e6504db6d2be37bc
wael3adel-lang <wael3adel@gmail.com>
2026-08-14 20:46:45 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,6 @@ self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addA
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys(),Promise.resolve()]).then(([keys])=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 
 self.addEventListener('fetch',e=>{
-  if(e.request.mode==="navigate" && new URL(e.request.url).pathname.endsWith("/index.html"))
-    return e.respondWith(fetch(e.request).then(r=>r||caches.match('/index.html')));
+  if(e.request.mode==="navigate") return e.respondWith(fetch(e.request).then(r=>r||caches.match('/index.html')));
   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
