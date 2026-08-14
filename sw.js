9f91812b1a9dc870a486070217c6a473151c2a05
wael3adel-lang <wael3adel@gmail.com>
2026-08-14 21:41:09 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,71 @@
-const CACHE='halab-fuel-v4';
-const ASSETS=['./','./index.html','./admin.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE = 'halab-fuel-v5';
 
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(Promise.all([caches.keys(),Promise.resolve()]).then(([keys])=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+const ASSETS = [
+  './',
+  './index.html',
+  './admin.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
 
-self.addEventListener('fetch',e=>{
-  if(e.request.mode==="navigate") return e.respondWith(fetch(e.request).then(r=>r||caches.match('/index.html')));
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+
+  self.skipWaiting();
+});
+
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    Promise.all([
+      self.clients.claim(),
+
+      caches.keys().then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE)
+            .map(key => caches.delete(key))
+        )
+      )
+    ])
+  );
+});
+
+
+self.addEventListener('fetch', event => {
+
+  if (event.request.mode === 'navigate') {
+
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+
+          const copy = response.clone();
+
+          caches.open(CACHE).then(cache => {
+            cache.put('./index.html', copy);
+          });
+
+          return response;
+
+        })
+        .catch(() =>
+          caches.match('./index.html')
+        )
+    );
+
+    return;
+  }
+
+
+  event.respondWith(
+    fetch(event.request)
+      .catch(() =>
+        caches.match(event.request)
+      )
+  );
+
 });
